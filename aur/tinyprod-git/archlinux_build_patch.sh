5467b97be02037aacc25a4286d9f092fd66e729d
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2019-11-30 15:23:11 +0100

    archlinux patch works

--- aur/tinyprod-git/archlinux_build_patch.sh
+++ aur/tinyprod-git/archlinux_build_patch.sh
@@ -2,10 +2,12 @@
 
 
 #is enough
-# -I/usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/
-# -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/
+# -I/usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/ -> stdarg,stddef
+# -I/usr/include/linux/ -> stddef
+# -I/usr\include\ -> inttypes
+#  
 
-###################################################################################
+################################################################################### works
 #sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/include\/linux\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/python/message/Makefile.am
 #sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/include\/linux\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/python/packet/Makefile.am
 #sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/include\/linux\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/c/sf/Makefile.am
@@ -17,9 +19,11 @@
 
 
 ###################################################################################
-sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ /g' src/prod/tools/tinyos/python/message/Makefile.am
-sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ /g' src/prod/tools/tinyos/python/packet/Makefile.am
-sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ /g' src/prod/tools/tinyos/c/sf/Makefile.am
-sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ /g' src/prod/tools/tinyos/java/net/tinyos/packet/Makefile.am
-sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ /g' src/prod/tools/tinyos/java/net/tinyos/message/Makefile.am
-sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ /g' src/prod/tools/tinyos/java/net/tinyos/tools/Makefile.am
+sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/python/message/Makefile.am
+sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/python/packet/Makefile.am
+sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/c/sf/Makefile.am
+sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/java/net/tinyos/packet/Makefile.am
+sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/java/net/tinyos/message/Makefile.am
+sed -i 's/$(TFLAGS)/$(TFLAGS) -I\/usr\/lib\/gcc\/x86_64-pc-linux-gnu\/9.2.0\/include\/ -I\/usr\/include\/ /g' src/prod/tools/tinyos/java/net/tinyos/tools/Makefile.am
+
+
